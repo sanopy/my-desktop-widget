c09d3e414f7174685f0692837846aab0f269a89c
uoo38 <plus.wisteria38@gmail.com>
2017-06-22 15:08:41 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const electron = require('electron');
+const {app, BrowserWindow, dialog, Menu, Tray} = electron;
+const path = require('path');
+const url  = require('url');
+
+let mainWindow = null;
+let settingWindow = null;
+let tray = null;
+
+function createMainWindow() {
+  const screen = electron.screen;
+  let screenSize = screen.getPrimaryDisplay().size;
+  let imageHeight = 283;
+
+  // console.log(screenSize);
+
+  mainWindow = new BrowserWindow({
+    x: 0,
+    y: screenSize.height - imageHeight - 40,
+    width: 390,
+    height: 285,
+    // useContentSize: true,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true
+  });
+  // mainWindow.setIgnoreMouseEvents(true);
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // mainWindow.webContents.openDevTools();
+
+  tray = new Tray('./img/icon32.jpg');
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '設定',
+      click() {
+        // createSettingWindow();
+        dialog.showMessageBox(mainWindow, {
+          type: 'info',
+          buttons: ['OK'],
+          title: '準備中',
+          message: '準備中',
+          detail: '後日対応予定'
+        });
+      }
+    },
+    {
+      label: '終了',
+      click() {
+        app.quit();
+      }
+    }
+  ]);
+  tray.setToolTip('This is my desktop widget.');
+  tray.setContextMenu(contextMenu);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+function createSettingWindow() {
+  settingWindow = new BrowserWindow({
+    useContentSize: true,
+    frame: false
+  });
+
+  settingWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/settings.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  settingWindow.on('closed', () => {
+    settingWindow = null;
+  });
+}
+
+app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
---
+++ src/js/index.js
@@ -0,0 +1,46 @@
+const {remote} = require('electron');
+const {Menu, MenuItem}   = remote;
+
+const menu = new Menu();
+menu.append(new MenuItem({
+  label: 'ひめ',
+  click() {
+    document.getElementById('img').src = '../img/image001.jpg';
+  }
+}));
+menu.append(new MenuItem({
+  label: 'かまって',
+  click() {
+    document.getElementById('img').src = '../img/image002.jpg';
+  }
+}));
+menu.append(new MenuItem({
+  label: 'あっ嬉しい',
+  click() {
+    document.getElementById('img').src = '../img/image003.jpg';
+  }
+}));
+menu.append(new MenuItem({
+  label: 'ドン引き',
+  click() {
+    document.getElementById('img').src = '../img/image004.jpg';
+  }
+}));
+menu.append(new MenuItem({
+  label: 'きょとん',
+  click() {
+    document.getElementById('img').src = '../img/image005.jpg';
+  }
+}));
+
+window.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+  menu.popup(remote.getCurrentWindow());
+}, false);
+
+setInterval(function() {
+  var ballon = document.getElementById('balloon');
+  var date = new Date();
+  // ballon.textContent = date.toDateString();
+  ballon.textContent = date.toLocaleString();
+}, 100);
